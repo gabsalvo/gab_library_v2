eda6e895714856f03bf17f8f2d09961cc2cebb11
gab <g.salvo4@studenti.unipi.it>
2023-04-23 23:10:04 +0200

    Dopo una tediosissima configurazioen di mysql, tra bug, auth errate, software buggato, workbench non più up to date e un infinito debugging. Ho finalmente collegato la node app al nostro database su mysql

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'gabriele',
+    password: 'password',
+    database: 'library_v2',
+});
 
 
 app.get('/', (req,res) => {
-    res.send("Hello, Testing")
+
+    const sqlInsert = "INSERT INTO library_v2 (title, author) VALUES ('Test Titolo', 'Test Autore');"
+    db.query(sqlInsert, (err, result) =>{
+        res.send("Hello, Testing Database_V2");
+        console.log(res);
+    });
 })
 
 app.listen(3001, () => {
