62cd9e245aa3af89ff4cd2e61a76de974c892916
gab <g.salvo4@studenti.unipi.it>
2023-04-25 20:54:38 +0200

    Connesso front-end e back-end, ora bisogna solo aggiungere le RUD e abbiamo fintio la parte 'complessa' e ci possiamo dedicare a fare il front end più carino e ad aggiungere login e la pagina per visualizzare i libri

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,45 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const newBook = () => {
+    Axios.post("http://localhost:3001/api/newBook", {
+      title: title,
+      author: author,
+    }).then(() => {
+      alert("successful newBook");
+    });
+  };
+
   return (
     <div className="App">
-     <h1>Gab Library</h1>
-     
-     <div className='add_new_book'>
-     <label>Book Title</label> 
-     <input type="text" name="book_title" />
+      <h1>Gab Library</h1>
 
-     <label>Author</label>
-     <input type="text" name="book_author" />
+      <div className="add_new_book">
+        <label>Book Title</label>
+        <input
+          type="text"
+          name="title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
 
-     <button>New</button>
-     </div>
+        <label>Author</label>
+        <input
+          type="text"
+          name="author"
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        />
 
+        <button onClick={newBook}>New</button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,24 +1,48 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'gabriele',
-    password: 'password',
-    database: 'library_v2',
+  host: "localhost",
+  user: "gabriele",
+  password: "password",
+  database: "library_v2",
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
+app.get('/api/getBooks', (req, res) => {
+    const sqlSelect = 
+    "SELECT * FROM library_v2.library_v2;";
+    db.query(sqlSelect, (err, result) =>{
+        res.send("Hello, getBooks");
+        console.log(result);
+    });
+})
+
+
+app.post("/api/newBook", (req, res) => {
+  const title = req.body.title;
+  const author = req.body.author;
+  const sqlInsert = "INSERT INTO library_v2 (title, author) VALUES (?,?);";
+  db.query(sqlInsert, [title, author], (err, res) => {
+    console.log(res);
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Siamo sulla port 3001");
+});
+
+/*app.get('/api/newBook', (req,res) => {
 
     const sqlInsert = "INSERT INTO library_v2 (title, author) VALUES ('Test Titolo', 'Test Autore');"
     db.query(sqlInsert, (err, result) =>{
         res.send("Hello, Testing Database_V2");
         console.log("Changes on Database Done");
     });
-})
-
-app.listen(3001, () => {
-    console.log("Siamo sulla port 3001")
-});
+});*/
